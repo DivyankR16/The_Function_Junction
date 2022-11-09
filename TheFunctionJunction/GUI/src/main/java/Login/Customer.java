c3669785eef46af105b835cfcbfda8fce39efe45
Akash <akashkulkarni4444@gmail.com>
2022-11-09 21:02:05 +0530

    Signup added.

--- TheFunctionJunction/GUI/src/main/java/Login/Customer.java
+++ TheFunctionJunction/GUI/src/main/java/Login/Customer.java
@@ -6,10 +6,12 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.LocalDate;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.concurrent.Callable;
 
-public class Customer extends Person
+public class Customer extends Person implements Login
 {
     private String CustomerID;
     private String MembershipStatus; //New,Premium,Gold
@@ -20,14 +22,14 @@ public class Customer extends Person
 
     }
 
-    public Customer(String firstName, String lastName, String phoneNumber, String emailId, String loginId, String password, Date DOB, String membershipStatus) {
+    public Customer(String firstName, String lastName, String phoneNumber, String emailId, String loginId, String password, LocalDate DOB, String membershipStatus) {
         super(firstName, lastName, phoneNumber, emailId, loginId, password, DOB);
         this.MembershipStatus = membershipStatus;
         this.DateOfJoining = Calendar.getInstance();
         this.DateOfJoining.set(Calendar.HOUR_OF_DAY,0);
     }
 
-    public Customer(String firstName, String lastName, String phoneNumber, String emailId, String loginId, String password, Date DOB) {
+    public Customer(String firstName, String lastName, String phoneNumber, String emailId, String loginId, String password, LocalDate DOB) {
         super(firstName, lastName, phoneNumber, emailId, loginId, password, DOB);
         this.MembershipStatus = "New";
         this.DateOfJoining = Calendar.getInstance();
@@ -42,9 +44,10 @@ public class Customer extends Person
     public void setDateOfJoining(Calendar dateOfJoining){this.DateOfJoining = dateOfJoining;}
     @Override
     public String getPass(String loginID){
-        Connection conn= Connect.createConnection();
         String password="";
-        try{String query="Select password from customer where loginid=?";
+        try{
+            Connection conn= Connect.createConnection();
+            String query="Select password from customer where loginid=?";
             PreparedStatement preStatement=conn.prepareStatement(query);
             preStatement.setString(1,loginID);
             ResultSet rs=preStatement.executeQuery();
@@ -76,5 +79,78 @@ public class Customer extends Person
             Connect.closeConnection();
         }
     }
+    public boolean Availability(String LogID)
+    {
+        Connection con1 = Connect.createConnection();
+        try {
+            String query1 = "SELECT loginid FROM customer WHERE loginid=?";
+            PreparedStatement preStatement=con1.prepareStatement(query1);
+            preStatement.setString(1,LogID);
+            ResultSet rs1 = preStatement.executeQuery();
+            if (rs1.isBeforeFirst())
+            {
+                System.out.println("LoginId Already Exists.");
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        catch (SQLException e)
+        {
+            throw new RuntimeException(e);
+        }
+        finally {
+            Connect.closeConnection();
+        }
+    }
+    public void Signup(String Fname, String Lname,String PhNo,String Email,String LoginId,String Password,LocalDate dob,String cid)
+    {
+        this.setFirstName(Fname);
+        this.setLastName(Lname);
+        this.setPhoneNumber(PhNo);
+        this.setEmailId(Email);
+        this.setLoginId(LoginId);
+        this.setPassword(Password);
+        this.setDOB(dob);
+        this.setMembershipStatus("New");
+        this.DateOfJoining = Calendar.getInstance();
+        this.DateOfJoining.set(Calendar.HOUR_OF_DAY,0);
+        this.setCustomerID(cid);
+        Connection conn = Connect.createConnection();
+        if (Availability(LoginId))
+        {
+            try {
+                Connection c2 = Connect.createConnection();
+                String query2 = "INSERT INTO customer (LoginId,FirstName,LastName,PhoneNumber,EmailId,Password,DOB,CustomerID,MembershipStatus,DateOfJoining) VALUES (?,?,?,?,?,?,?,?,?,?)";
+                PreparedStatement preStatement2 = c2.prepareStatement(query2);
+                preStatement2.setString(1,this.getLoginId());
+                preStatement2.setString(2,this.getFirstName());
+                preStatement2.setString(3,this.getLastName());
+                preStatement2.setString(4,this.getPhoneNumber());
+                preStatement2.setString(5,this.getEmailId());
+                preStatement2.setString(6,this.getPassword());
+                preStatement2.setString(7,this.getDOB().toString());
+                preStatement2.setString(8,this.getCustomerID());
+                preStatement2.setString(9,this.getMembershipStatus());
+                preStatement2.setString(10,this.getDateOfJoining().toString());
+//                preStatement2.executeUpdate();
+                preStatement2.execute();
 
+            }
+            catch (SQLException e)
+            {
+                e.printStackTrace();
+            }
+            finally
+            {
+                Connect.closeConnection();
+            }
+        }
+        else
+        {
+            System.out.println("unsuccessfull");
+        }
+    }
 }
