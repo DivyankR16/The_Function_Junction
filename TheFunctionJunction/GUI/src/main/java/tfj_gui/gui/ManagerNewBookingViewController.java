b4caca6168752d54f5749d5f639c4ca0d90739fe
Akash <akashkulkarni4444@gmail.com>
2022-11-10 17:04:45 +0530

    Manager Booking Added.

---
+++ TheFunctionJunction/GUI/src/main/java/tfj_gui/gui/ManagerNewBookingViewController.java
@@ -0,0 +1,74 @@
+package tfj_gui.gui;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+import java.io.IOException;
+
+public class ManagerNewBookingViewController
+{
+    @FXML
+    private Button HomeButton;
+    @FXML
+    private Button NewBookingButton;
+    @FXML
+    private Button MyAccountButton;
+    @FXML
+    private Button MyBookingsButton;
+    @FXML
+    private Button ChangePasswordButton;
+    @FXML
+    private Button AboutUsButton;
+    @FXML
+    private Button HelpButton;
+    @FXML
+    private Button LogoutButton;
+    @FXML
+    private Button ExitButton;
+    @FXML
+    protected void GoToHome(ActionEvent event) throws IOException
+    {
+        ManagerControllerFunctions.GoToHome(event);
+    }
+    @FXML
+    protected void NewBookingButtonClicked(ActionEvent event) throws IOException
+    {
+        ManagerControllerFunctions.NewBookingButtonClicked(event);
+    }
+    @FXML
+    protected void MyAccountButtonClicked(ActionEvent event) throws IOException
+    {
+        ManagerControllerFunctions.MyAccountButtonClicked(event);
+    }
+    @FXML
+    protected void MyBookingsButtonClicked(ActionEvent event) throws IOException
+    {
+        ManagerControllerFunctions.MyBookingsButtonClicked(event);
+    }
+    @FXML
+    protected void ChangePasswordButtonClicked(ActionEvent event) throws IOException
+    {
+        ManagerControllerFunctions.ChangePasswordButtonClicked(event);
+    }
+    @FXML
+    protected void AboutUsButtonClicked(ActionEvent event) throws IOException
+    {
+        ManagerControllerFunctions.AboutUsButtonClicked(event);
+    }
+    @FXML
+    protected void HelpButtonClicked(ActionEvent event) throws IOException
+    {
+        ManagerControllerFunctions.HelpButtonClicked(event);
+    }
+    @FXML
+    protected void LogoutButtonClicked(ActionEvent event) throws IOException {
+        ManagerControllerFunctions.LogoutButtonClicked(event);
+    }
+    @FXML
+    protected void ExitButtonClicked(ActionEvent event) throws IOException
+    {
+        ManagerControllerFunctions.ExitButtonClicked(event);
+    }
+
+}
