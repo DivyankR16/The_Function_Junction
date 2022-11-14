44d049804d76b94c69964375492f2888ecdb8703
Akash <akashkulkarni4444@gmail.com>
2022-11-14 17:58:22 +0530

    New Booking

--- TheFunctionJunction/GUI/src/main/java/tfj_gui/gui/ControllerFunctions.java
+++ TheFunctionJunction/GUI/src/main/java/tfj_gui/gui/ControllerFunctions.java
@@ -4,6 +4,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
