77474c959f485ba258aca84950f7c2316283032e
Anurag <10anuragyadav2002@gmail.com>
2022-11-18 20:20:00 +0530

    My Account Updated.

--- TheFunctionJunction/GUI/src/main/java/tfj_gui/gui/InfoOfAccountViewController.java
+++ TheFunctionJunction/GUI/src/main/java/tfj_gui/gui/InfoOfAccountViewController.java
@@ -1,22 +1,41 @@
 package tfj_gui.gui;
 
+import Database.DBconnection.Connect;
 import Login.*;
 
 import Login.Customer;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
-import javafx.scene.Scene;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.*;
+import java.util.Date;
+import java.util.ResourceBundle;
 
-public class InfoOfAccountViewController
-{
+import static Database.DBconnection.Connect.con;
+import static Database.DBconnection.Connect.createConnection;
 
+public class InfoOfAccountViewController implements Initializable {
+    @FXML
+    public  Label PhoneNumber;
+    @FXML
+    public  Label Email;
+    @FXML
+    public Label MembershipStatus;
+    @FXML
+    public Label LoginId;
+    @FXML
+    public Label Age;
     @FXML
     private Button HomeButton;
     @FXML
@@ -35,57 +54,98 @@ public class InfoOfAccountViewController
     private Button LogoutButton;
     @FXML
     private Button ExitButton;
+
     @FXML
-    private Label Name;
-    @FXML
-    private void abcd()
-    {
-        Customer c1 = new Customer();
-        Name.setText(c1.getFirstName()+" "+c1.getLastName());
+    protected void GoToHome(ActionEvent event) throws IOException {
+        ControllerFunctions.GoToHome(event);
     }
 
     @FXML
-    protected void GoToHome(ActionEvent event) throws IOException
-    {
-        ControllerFunctions.GoToHome(event);
-    }
-    @FXML
-    protected void NewBookingButtonClicked(ActionEvent event) throws IOException
-    {
+    protected void NewBookingButtonClicked(ActionEvent event) throws IOException {
         ControllerFunctions.NewBookingButtonClicked(event);
     }
+
     @FXML
-    protected void MyAccountButtonClicked(ActionEvent event) throws IOException
-    {
+    protected void MyAccountButtonClicked(ActionEvent event) throws IOException, SQLException {
         ControllerFunctions.MyAccountButtonClicked(event);
     }
+
     @FXML
-    protected void MyBookingsButtonClicked(ActionEvent event) throws IOException
-    {
+    protected void MyBookingsButtonClicked(ActionEvent event) throws IOException {
         ControllerFunctions.MyBookingsButtonClicked(event);
     }
+
     @FXML
-    protected void ChangePasswordButtonClicked(ActionEvent event) throws IOException
-    {
+    protected void ChangePasswordButtonClicked(ActionEvent event) throws IOException {
         ControllerFunctions.ChangePasswordButtonClicked(event);
     }
+
     @FXML
-    protected void AboutUsButtonClicked(ActionEvent event) throws IOException
-    {
+    protected void AboutUsButtonClicked(ActionEvent event) throws IOException {
         ControllerFunctions.AboutUsButtonClicked(event);
     }
+
     @FXML
-    protected void HelpButtonClicked(ActionEvent event) throws IOException
-    {
+    protected void HelpButtonClicked(ActionEvent event) throws IOException {
         ControllerFunctions.HelpButtonClicked(event);
     }
+
     @FXML
     protected void LogoutButtonClicked(ActionEvent event) throws IOException {
         ControllerFunctions.LogoutButtonClicked(event);
     }
+
     @FXML
-    protected void ExitButtonClicked(ActionEvent event) throws IOException
-    {
+    protected void ExitButtonClicked(ActionEvent event) throws IOException {
         ControllerFunctions.ExitButtonClicked(event);
     }
+
+    @FXML
+    private Label Name;
+
+
+    //Connect JDBC
+
+    //search the database for required login id
+//    @FXML
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        Send_Data_Between need = Send_Data_Between.getInstance();
+            Customer c1 = need.getCustomer();
+            String LoginID = c1.getLoginId();
+        try {
+            Connection con = Connect.createConnection();
+            String query1 = "SELECT  * FROM customer WHERE loginid=?";
+            PreparedStatement preStatement = con.prepareStatement(query1);
+            preStatement.setString(1, LoginID);
+            ResultSet rs1 = preStatement.executeQuery();
+            if (rs1.next()) {
+                Name.setText(rs1.getString(3));
+                PhoneNumber.setText(rs1.getString(5));
+                Email.setText(rs1.getString(6));
+                MembershipStatus.setText(rs1.getString(9));
+                LoginId.setText(rs1.getString(1));
+                SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+                Date date = formatter.parse(rs1.getString(7));
+                Instant instant = date.toInstant();
+                ZonedDateTime zone = instant.atZone(ZoneId.systemDefault());
+                LocalDate localDate = zone.toLocalDate();
+                Period period = Period.between(localDate, LocalDate.now());
+                String age = String.valueOf(period.getYears());
+                Age.setText(age);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        } finally {
+            Connect.closeConnection();
+            try {
+                con.close();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
 }
--- TheFunctionJunction/GUI/src/main/java/tfj_gui/gui/ManagerInfoOfAccountViewController.java
+++ TheFunctionJunction/GUI/src/main/java/tfj_gui/gui/ManagerInfoOfAccountViewController.java
@@ -1,13 +1,41 @@
 package tfj_gui.gui;
 
+import Database.DBconnection.Connect;
+import Login.Customer;
+import Login.Manager;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 
 import java.io.IOException;
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.*;
+import java.util.Date;
+import java.util.ResourceBundle;
 
-public class ManagerInfoOfAccountViewController
-{
+import static Database.DBconnection.Connect.con;
+
+public class ManagerInfoOfAccountViewController implements Initializable
+{   @FXML
+    public Label Name;
+    @FXML
+    public Label PhoneNumber;
+    @FXML
+    public Label Email;
+    @FXML
+    public Label MembershipStatus;
+    @FXML
+    public Label LoginId;
+    @FXML
+    public Label Age;
     @FXML
     private Button HomeButton;
     @FXML
@@ -69,5 +97,45 @@ public class ManagerInfoOfAccountViewController
     protected void ExitButtonClicked(ActionEvent event) throws IOException
     {
         ManagerControllerFunctions.ExitButtonClicked(event);
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        Send_Data_Between need = Send_Data_Between.getInstance();
+        Manager m1 = need.getManager();
+        String LoginID = m1.getLoginId();
+        try {
+            Connection con = Connect.createConnection();
+            String query1 = "SELECT  * FROM manager WHERE loginid=?";
+            PreparedStatement preStatement = con.prepareStatement(query1);
+            preStatement.setString(1, LoginID);
+            ResultSet rs1 = preStatement.executeQuery();
+            if (rs1.next()) {
+                Name.setText(rs1.getString(1));
+                PhoneNumber.setText(rs1.getString(3));
+                Email.setText(rs1.getString(4));
+                MembershipStatus.setText("Manager");
+                LoginId.setText(rs1.getString(5));
+                SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+                Date date = formatter.parse(rs1.getString(7));
+                Instant instant = date.toInstant();
+                ZonedDateTime zone = instant.atZone(ZoneId.systemDefault());
+                LocalDate localDate = zone.toLocalDate();
+                Period period = Period.between(localDate, LocalDate.now());
+                String age = String.valueOf(period.getYears());
+                Age.setText(age);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        } finally {
+            Connect.closeConnection();
+            try {
+                con.close();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 }
